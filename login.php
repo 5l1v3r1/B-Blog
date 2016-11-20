f579014aada9c5aafae795a7aa8be15e27a3fd24
amirhosseinkarimi <amir.hossein7464@gmail.com>
2016-11-20 12:02:20 +0330

    Login user basically

--- login.php
+++ login.php
@@ -1,12 +1,18 @@
 <?php
+	/**
+	*	Login/Logout page
+	*	@param $_POST['USER']	Username
+	*	@param $_POST['PASS']	User password
+	*	@param $_GET['OUT']		Logout user
+	*	return values
+	*	@return	0	invalid data
+	*	@return	1	Loged out
+	*	@return	2	User/pass is wrong
+	*	@return	200	OK, Loged in
+	*/
 	require_once('includes/functions.php');
 
-	// for test directly
-	$_POST['A'] = 'testuser';
-	$_POST['USER'] = 'testuser';
-	$_POST['PASS'] = '12345678';
-
-	$R = 0;	// something went wrong
+	$R = 0;
 
 	if( !logedin() AND ISSET($_POST['USER'],$_POST['PASS']) )
 	{
@@ -18,23 +24,36 @@
 			$PASS = mkhash($PASS);
 
 			$GET_USER = $SQL->query("SELECT ID,USER,NAME,EMAIL,WEB,GRP FROM USERS WHERE USER='".scape($USER)."' AND PASS='".scape($PASS)."'");
+
+			if( $GET_USER!==FALSE && $GET_USER->num_rows==1 )
+			{
+				$USER = $GET_USER->fetch_assoc();
+
+				foreach ($USER as $key => $value) {
+					$_SESSION[$key] = $value;
+				}
+
+				$R = 200;
+			}
+			else {
+				$R = 2;
+			}
 		}
 		else {
-			$R = 2; // Wrong user/pass
+			$R = 2;
 		}
 	}
-	else
-	{
+	else {
 		if( ISSET($_GET['OUT']) )
 		{
 			session_destroy();
 			session_start();
-			$R = 1; // logouted
+			$R = 1;
 		}
 	}
 
 	if( !isset($_POST['A']) )
-		//header("Location: index.php");
+		header("Location: index.php");
 
 	echo $R;
 ?>
